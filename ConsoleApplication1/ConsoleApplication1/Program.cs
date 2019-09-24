55d0e7b2f43ced809271e1cf5dae1b97ad59708c
YoloChocoPie <chocopieyolo@gmail.com>
2019-09-24 15:32:44 +0700

    UPDATE 20/09

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,56 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //Console.WriteLine("Hello World");
+            HLine(10, '*'); Console.WriteLine();
+            HLine(10, 'a'); Console.WriteLine();
+            HLine(10, 'b'); Console.WriteLine();
+
+            VLine(10, '*'); Console.WriteLine();
+            DLine(1,  '*'); Console.WriteLine();
+            DLine(2,  '*'); Console.WriteLine();
+            DLine(3,  '*'); Console.WriteLine();
+            DLine(4,  '*'); Console.WriteLine();
+            DrawTriangle(2, '*');
+
         }
+
+        static void HLine(int n, char c)
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write(c);
+        }
+
+        static void VLine(int n, char c)
+        {
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(c);
+        }
+
+        static void DLine(int n, char c)
+        {
+            for (int i = 0; i < n; i++)
+                HLine(i, ' ');
+                Console.WriteLine(" " + c);
+        }
+
+        static void DrawTriangle(int n, char c) 
+        { 
+                // first line 
+            HLine(n - 1, ' '); HLine(1, c); Console.WriteLine();
+            int a = 2, b = 1;
+            for (int i = 0; i < n - 2; i++)
+            {
+                HLine(n - a, ' ');
+                // fifth line
+                a = a + 1;
+                b = b + 2;
+            }
+                // last line
+            HLine(2 * n - 1, '*'); Console.WriteLine();
+        }
+        
+            
+
     }
 }
